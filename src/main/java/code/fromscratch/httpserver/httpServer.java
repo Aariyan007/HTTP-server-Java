9a6a6f98665a947fd0a725576b6f053c49faaa4f
Aariyan007 <aariyansunu28@gmail.com>
2026-05-15 23:34:09 +0530

    Initial commit

---
+++ src/main/java/code/fromscratch/httpserver/httpServer.java
@@ -0,0 +1,7 @@
+package code.fromscratch.httpserver;
+//Driver class for HTTP server
+public class httpServer {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
